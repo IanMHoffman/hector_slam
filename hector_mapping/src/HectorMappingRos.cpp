bd466050e943acba64ddc0ba15b0a63d07d990f6
GamePlayNextGen <Ian.Michael.Hoffman@gmail.com>
2014-04-02 00:54:12 -0600

    Temporary Fix For Transform Error
    Temporary fix for the following errors:
    
    [ERROR] Extrapolation Error: Unable to lookup transform, cache is empty, when looking up transform from frame [/odom] to frame [/map]
    [ERROR] Global Frame: /odom Plan Frame size 98: /map
    [ WARN] Could not transform the global plan to the frame of the controller

--- hector_mapping/src/HectorMappingRos.cpp
+++ hector_mapping/src/HectorMappingRos.cpp
@@ -344,7 +344,8 @@ void HectorMappingRos::scanCallback(const sensor_msgs::LaserScan& scan)
       odom_to_base.setIdentity();
     }
     map_to_odom_ = tf::Transform(poseInfoContainer_.getTfTransform() * odom_to_base.inverse());
-    tfB_->sendTransform( tf::StampedTransform (map_to_odom_, scan.header.stamp, p_map_frame_, p_odom_frame_));
+    ros::Duration future_date_duration(0.3F);
+tfB_->sendTransform( tf::StampedTransform (map_to_odom_, scan.header.stamp  + future_date_duration, p_map_frame_, p_odom_frame_));
   }
 
   if (p_pub_map_scanmatch_transform_){
